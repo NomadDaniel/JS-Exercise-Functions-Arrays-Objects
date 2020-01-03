ad7a491d55540b5b0aba4ef4b9514897a513ef22
NomadDaniel <reachingdaniel@gmail.com>
2020-01-03 21:56:12 +0000

    passed 2

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
-}
+function makePersonObject(id, name, email) {
+            return {id, name, email};
+}   
+
+console.log(makePersonObject(5, 'Leia', 'leia.com'))
 
 /**
  * ### Challenge `getName`
