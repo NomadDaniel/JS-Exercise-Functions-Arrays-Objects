2e95cd144206a46d4e2270486866f0cf06c29be9
NomadDaniel <reachingdaniel@gmail.com>
2020-01-03 23:44:56 +0000

    passed 6

--- index.js
+++ index.js
@@ -77,9 +77,15 @@ console.log(getName('Leia'))
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
-}
+function makeSmartPerson (name) {
+    return { name: name,
+            sum: function (num1,num2){
+                return num1+num2},
+            speak: function (){
+                return `Hello, my name is ${name}`;
+            }
+} 
+}  
 
 
 
