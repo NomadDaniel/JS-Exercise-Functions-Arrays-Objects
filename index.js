d2f70a651572ddf6ba441d6f1890c7ccfa221129
NomadDaniel <reachingdaniel@gmail.com>
2020-01-04 01:49:36 +0000

    17 passed

--- index.js
+++ index.js
@@ -202,8 +202,14 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, maxYear) {
+  let findOldCars = [];
+    for (var i = 0; i < inventory.length; i++) {
+        if (inventory[i].car_year <= maxYear) {
+            findOldCars.push(inventory[i]);
+  }
+}
+  return findOldCars;
 }
 
 /**
