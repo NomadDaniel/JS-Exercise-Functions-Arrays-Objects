93d4bb5e7889e1256926082e6e1ec696ceb20adc
NomadDaniel <reachingdaniel@gmail.com>
2020-03-21 19:06:23 -0400

    14 passing

--- index.js
+++ index.js
@@ -217,8 +217,13 @@ function sortCarInventory(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  const newArr = [];
+
+  for (let i=0; i<inventory.length; i++) {
+    newArr.push(inventory[i].car_year);
+  }
+  return newArr;  
 }
 
 /**
