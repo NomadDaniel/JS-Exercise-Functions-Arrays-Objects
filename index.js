fbc77fcbaa86bef6b8ca8e758281f6710e85de76
NomadDaniel <reachingdaniel@gmail.com>
2020-03-21 19:14:22 -0400

    17 passing

--- index.js
+++ index.js
@@ -238,8 +238,16 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, year) {
+  const olderCar = [];
+  
+  for (let i=0; i<inventory.length; i++) {
+  
+    if(inventory[i].car_year <= year) {
+      olderCar.push(inventory[i]);
+    }
+}
+return olderCar;
 }
 
 /**
@@ -253,8 +261,8 @@ function getOlderCars(/* code here */) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  
 }
 
 /**
